fdf0f1dc243481ce2d669b4cf3841c202ec3a53f
鸭脖yaboo <863176846@qq.com>
2022-11-13 11:24:56 +0800

    chore(husky): add commit-msg hook

---
+++ commitlint.config.js
@@ -0,0 +1,27 @@
+/**
+* feature：新功能
+* update：更新某功能
+* fixbug：修补某功能的bug
+* refactor：重构某个功能
+* optimize: 优化构建工具或运行时性能
+* style：仅样式改动
+* docs：仅文档新增/改动
+* chore：构建过程或辅助工具的变动
+*/
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feature', 'update', 'fixbug', 'refactor', 'optimize', 'style', 'docs', 'chore'
+    ]],
+    'type-case': [0],
+    'type-empty': [0],
+    'scope-empty': [0],
+    'scope-case': [0],
+    'subject-full-stop': [0, 'never'],
+    'subject-case': [0, 'never'],
+    'header-max-length': [0, 'always', 72]
+  }
+};
